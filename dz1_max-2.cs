89f47f909c213f44c8495d5e98a05f4664bce138
RomanBusygin <roman.alex.95@mail.ru>
2022-06-29 23:19:25 +0500

    ДЗ по семинару 1

---
+++ dz1_max-2.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+int min = 0;
+if (number1 > number2)
+{
+    max = number1;
+    min = number2;
+    Console.Write("Первое число больше, чем второе");
+}
+else if (number1 < number2)
+{
+    max = number2;
+    min = number1;
+    Console.Write("Второе число больше, чем первое");
+}
+else Console.Write("Числа равны");
---
+++ dz4_parityFromTo.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+if (N > 0)
+{
+    while (i < N + 1)
+    {
+        Console.Write(i + " ");
+        i = i + 2;
+    }
+}
+else Console.Write("Число слишком мало");
